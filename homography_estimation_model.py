60d45958f419b152ee574b119b62e1955af95009
rodrigocastillogl <rodrigocastillogl@gmail.com>
2023-04-25 18:54:52 -0500

    feat: define forward().

--- homography_estimation_model.py
+++ homography_estimation_model.py
@@ -25,66 +25,87 @@ class HomographyModel(nn.Module):
         
         super().__init__()
 
+        # ------------ Convolutional layers -----------
         self.layer1 = nn.Sequential(
             nn.Conv2d(2, 64, 3, padding = 'same') ,
             nn.ReLU()                             ,
             nn.BatchNorm2d(64)
         )
-
         self.layer2 = nn.Sequential(
             nn.Conv2d(64, 64, 3, padding = 'same') ,
             nn.ReLU()                              ,
             nn.BatchNorm2d(64)                     ,
             nn.MaxPool2d(2)
         )
-
         self.layer3 = nn.Sequential(
             nn.Conv2d(64, 64, 3, padding = 'same') ,
             nn.ReLU()                              ,
             nn.BatchNorm2d(64)
         )
-
         self.layer4 = nn.Sequential(
             nn.Conv2d(64, 64, 3, padding = 'same') ,
             nn.ReLU()                              ,
             nn.BatchNorm2d(64)                     ,
             nn. MAxPool2d(2)
         )
-        
         self.layer5 = nn.Sequential(
             nn.Conv2d(64, 128, 3, padding = 'same') ,
             nn.ReLU()                               ,
             nn.BatchNorm2d(128)
         )
-        
         self.layer6 = nn.Sequential(
             nn.Conv2d(128, 128, 3, padding = 'same') ,
             nn.ReLU()                                ,
             nn.BatchNorm2d(128)                      ,
             nn. MAxPool2d(2)
         )
-        
         self.layer7 = nn.Sequential(
             nn.Conv2d(128, 128, 3, padding = 'same') ,
             nn.ReLU()                                ,
             nn.BatchNorm2d(128)
         )
-        
         self.layer8 = nn.Sequential(
             nn.Conv2d(128, 128, 3, padding = 'same') ,
             nn.ReLU()                                ,
             nn.BatchNorm2d(128)
         )
-
-        self.drop1 = nn.Dropout2d(0.5)
+        # ---------------------------------------------
+        
+        # ------ Fully connected + drop out -----------
+        self.drop1 = nn.Dropout(0.5)
         self.fc1 = nn.Linear(128*16*16, 1024)
         self.drop2 = nn.Dropout(0.5)
         self. fc2 = nn.Linear(1024, 8)
+         # ---------------------------------------------
     
-    def forward(self):
+    def forward(self, x):
         """
         Forward pass.
         Input
         -----
+            * x : input (2 channel image).
         """
-        pass
+
+        # --- Convolutional layers ---
+        out = self.layer1(x)
+        out = self.layer2(x)
+        out = self.layer3(x)
+        out = self.layer4(x)
+        out = self.layer5(x)
+        out = self.layer6(x)
+        out = self.layer7(x)
+        out = self.layer8(x)
+        # ----------------------------
+ 
+        # Flatten
+        out = out.view(-1, 128*16*16, 1024)
+
+        # --- Fully connected + drop out ---
+        out = self.drop1(out)
+        out = self.fc1(out)
+        out = self.drop2(out)
+        out = self.fc2(out)
+        # ----------------------------------
+        
+        return out
+        
