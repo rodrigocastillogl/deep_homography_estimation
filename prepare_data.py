d4b16342d213a24a4937fee28f8702138f77c28d
rodrigocastillogl <rodrigocastillogl@gmail.com>
2023-05-18 11:20:53 -0600

    feat: Add function to download dataset (zip file) and unzip it (prepare_data.py).

--- prepare_data.py
+++ prepare_data.py
@@ -1,62 +1,79 @@
-import cv2
-import numpy as np
 import os
+from glob import glob
+#from shutil import rmtree
+import errno
+import wget
+import zipfile
 
-def img_processing(img_path):
-
-    '''
-    Image processing function.
-
+def download_data( data_url = None, data_directory = None ):
+    """
+    Download dataset.
     Input
+    -----
+        * data_url: url to download dataset.
+        * data_directory : diretory to save downloaded file.
+    Output
     ------
-        * img_path : dataset path.
+        *  None
+    """
 
-    Return
-    ------
-        * processed : processed 2-channel image
-        * diff      : homography parameterized by point differences.
+    assert data_url is not None
+    assert data_directory is not None
 
-    '''
+    print(f'Downloading dataset (it may take a while)...')
+    response = wget.download(data_url, data_directory)
+    print(f'Save as {data_directory}')
+
+
+def split_data( data_path = None, ratio = 0.8 ):
+    pass
     
 
-    WIDTH      = 320    # image width
-    HEIGHT     = 240    # image height
-    PATCH_SIZE = 128    # patch size 
-    RHO        = 32     # disturbance range
 
-    # Read and resize grayscale image
-    img = cv2.imread( img_path , 0)
-    img = cv2.resize( img , (WIDTH, HEIGHT) )
-    img = (img - 127.5)/127.5
-    
-    # Create a random square in the image
-    points    = np.array( [ [ RHO, RHO ]                         ,
-                          [ PATCH_SIZE + RHO, RHO ]              ,
-                          [ RHO, PATCH_SIZE + RHO ]              ,
-                          [ PATCH_SIZE + RHO, PATCH_SIZE + RHO ] ] )
-    a = np.random.randint( low  = 1, high = WIDTH - 2 * RHO - PATCH_SIZE )
-    b = np.random.randint( low  = 1, high = HEIGHT - 2 * RHO - PATCH_SIZE )
-    points   += np.array( [[a,b]] )
-    
-    # Randomly perturb the corners within the range [-rho, rho] 
-    moved_points = points + np.random.randint( low  = - RHO ,
-                                               high = RHO   ,
-                                               size = (4,2) )
-    
-    # Compute homography
-    H = cv2.getPerspectiveTransform( points.astype('float32')       ,
-                                     moved_points.astype('float32') )
-    
-    # Compute inverse
-    H_inverse = np.linalg.inv(H)
+if __name__ == '__main__':
 
-    # Apply H to image
-    warped_image = cv2.warpPerspective(img, H_inverse, (WIDTH, HEIGHT) )
+    data_url = 'http://images.cocodataset.org/zips/train2017.zip'
 
-    I1 = img[ points[0,1]:points[2,1] , points[0,0]:points[1,0] ]
-    I2 = warped_image[ points[0,1]:points[2,1] , points[0,0]:points[1,0] ]
 
-    # H parameterized by differences
-    diff = (moved_points - points).reshape(8)
+    dataset_directory = 'data'
+    train_directory = dataset_directory + '/train'
+    test_directory  = dataset_directory + '/test'
 
-    return np.stack( [I1, I2], axis = 2 ), diff
+    # create dataset directory
+    try:
+        os.makedirs(dataset_directory)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+    if os.path.exists(train_directory) and os.path.exists(test_directory):
+        # if train and test directories already exists:
+        #   show a message and do nothing
+        print( 'The dataset already exists at ' + train_directory +
+               'and' + test_directory  )
+    else:
+
+        # download dataset zip file
+        zip_directory = dataset_directory + "/ms-coco.zip"
+        download_data(data_url, dataset_directory)
+
+        # extract fata from zip file
+        print('Extracting MS-COCO 2017 dataset ...')
+        with zipfile.ZipFile(zip_directory, 'r') as archive:
+            archive.extractall(dataset_directory)
+        # remove zip file
+        os.remove(zip_directory)
+
+
+        # create train and test directories
+        try:
+            os.makedirs(train_directory)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+        
+        try:
+            os.makedirs(test_directory)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
